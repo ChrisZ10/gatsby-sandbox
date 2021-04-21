d55d3ad08966eb1055883da99ce537d92aeac6ea
Chris Zhao <chrislinzhao@gmail.com>
2021-04-21 13:06:42 -0400

    update

---
+++ src/templates/blog.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+
+import Layout from '../components/layout';
+
+export const query = graphql`
+    query($slug: String!) {
+        markdownRemark(fields: {
+            slug: {
+                eq: $slug
+            }
+        }) {
+            frontmatter {
+                Title
+                Date
+            }
+            html
+        }
+    }
+`;
+
+const Blog = ({ data }) => {
+    return (
+        <Layout>
+            <h1>{data.markdownRemark.frontmatter.Title}</h1>
+            <h2>{data.markdownRemark.frontmatter.Date}</h2>
+            <div dangerouslySetInnerHTML={{ __html : data.markdownRemark.html }}></div>
+        </Layout>
+    );
+};
+
+export default Blog;
